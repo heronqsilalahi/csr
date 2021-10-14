7342bd400ec5b28f11d074843268a831add24a9f
766hi <heronqsilalahi@gmail.com>
2021-10-14 22:19:04 +0700

    Update generate_csr.sh

--- generate_csr.sh
+++ generate_csr.sh
@@ -92,10 +92,10 @@ openssl req -in $ssl_directory/$domain/$fqdn.csr -noout -text
 chmod 400 $ssl_directory/$domain/$fqdn.key
 
 set +x
-echo "${cyan}${bgwhite}================= Your Private Key :$ssl_directory/$domain/$fqdn.key ===================${reset}${green}"
+echo "${cyan}${bgyellow}================= Your Private Key :$ssl_directory/$domain/$fqdn.key ===================${reset}${green}"
 cat $ssl_directory/$domain/$fqdn.key
 echo "${reset}"
-echo "${cyan}${bgwhite}================= Your CSR  :$ssl_directory/$domain/$fqdn.csr ==================${reset}${green}"
+echo "${cyan}${bgyellow}================= Your CSR  :$ssl_directory/$domain/$fqdn.csr ==================${reset}${green}"
 cat $ssl_directory/$domain/$fqdn.csr
 echo "${reset}"
 
