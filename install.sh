c7d6326b00c7abbfc6d73d37dff9e4b9369d49ae
766hi <heronqsilalahi@gmail.com>
2022-06-21 19:14:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
+echo "Install Generate-CSR scipt"
+set -x
 ssl_directory="/tmp/openssl"
+# Remove previous version
+rm -rf $ssl_directory
+rm -rf /usr/local/bin/Generate-CSR
 mkdir -p $ssl_directory
-chown 777 -R /tmp
+chmod 777 -R $ssl_directory
 wget https://raw.githubusercontent.com/heronqsilalahi/csr/main/csr.conf -O $ssl_directory/.conf
 wget https://raw.githubusercontent.com/heronqsilalahi/csr/main/generate_csr.sh -O /usr/local/bin/Generate-CSR
 sudo chmod +x /usr/local/bin/Generate-CSR
+set +x
 echo "Script install successfully"
 echo "Run command 'Generate-CSR' to start! \n"
 echo " "
