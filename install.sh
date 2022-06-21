0703429d2e7a055293ffa5bded1b76d9d1325ae5
766hi <heronqsilalahi@gmail.com>
2022-06-21 17:51:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ mkdir -p $ssl_directory
 wget https://raw.githubusercontent.com/heronqsilalahi/csr/main/csr.conf -O $ssl_directory/.conf
 wget https://raw.githubusercontent.com/heronqsilalahi/csr/main/generate_csr.sh -O /usr/local/bin/Generate-CSR
 sudo chmod +x /usr/local/bin/Generate-CSR
+echo "Script install successfully"
+echo "Run command 'Generate-CSR' to start!"
