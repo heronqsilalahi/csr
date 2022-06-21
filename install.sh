b7c37673943da19c15c6ee0950f91f61aa752142
766hi <heronqsilalahi@gmail.com>
2022-06-21 18:54:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 ssl_directory="/tmp/openssl"
 mkdir -p $ssl_directory
-chown 755 $ssl_directory
+chown 777 -R /tmp
 wget https://raw.githubusercontent.com/heronqsilalahi/csr/main/csr.conf -O $ssl_directory/.conf
 wget https://raw.githubusercontent.com/heronqsilalahi/csr/main/generate_csr.sh -O /usr/local/bin/Generate-CSR
 sudo chmod +x /usr/local/bin/Generate-CSR
