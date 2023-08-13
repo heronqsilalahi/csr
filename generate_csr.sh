ca993713e5f9b18be026a05293d593cdb1cf882e
766hi <heronqsilalahi@gmail.com>
2023-08-13 18:07:42 +0700

    Update generate_csr.sh

--- generate_csr.sh
+++ generate_csr.sh
@@ -133,7 +133,8 @@ then
 	echo "${cyan}Your CSR  :$ssl_directory/$domain/$hostname.csr ${reset}${green}"
 	cat $ssl_directory/$domain/$hostname.csr
 	echo "${reset}"
-	cd $ssl_directory/$domain
+	cd $ssl_directory
+ 	cd $domain
 
 #Create PFX Ceertificate
 elif [ "$action" = "2" ] 
